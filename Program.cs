4fee8f96e2764c241d8b4c08780c82eada445181
Alexisz3 <kapatino@sudamericano.edu.ec>
2026-05-24 11:14:33 -0500

    Migrate from InMemory to SQLite

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseInMemoryDatabase("ProductsDb"));
+    options.UseSqlite("Data Source=productos.db"));
 
 var app = builder.Build();
 
--- obj/Debug/net10.0/ApiRestCrud.AssemblyInfo.cs
+++ obj/Debug/net10.0/ApiRestCrud.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiRestCrud")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2958edf2028988e6ef9e7d8c5a726b02d68b1f15")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiRestCrud")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiRestCrud")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
